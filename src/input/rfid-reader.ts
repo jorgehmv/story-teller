a814e2c00ff8701ef422229fe7664918d9d918f1
Jorge Martinez <jorgehmv@gmail.com>
2023-02-22 11:31:54 -0600

    more testing

--- src/input/rfid-reader.ts
+++ src/input/rfid-reader.ts
@@ -4,14 +4,28 @@ import { SerialPort } from "serialport";
 export class RfidReader implements IReader {
   async read(): Promise<string> {
     console.log("entered read");
-    var sp = new SerialPort({
-      path: process.env.USB_SERIAL_PATH!,
-      baudRate: 4800,
+    console.log(`opening at ${process.env.USB_SERIAL_PATH}`);
+    var port = new SerialPort(
+      {
+        path: process.env.USB_SERIAL_PATH!,
+        baudRate: 57600,
+      },
+      () => {
+        console.log(`open callback: ${arguments}`);
+      }
+    );
+
+    port.on("open", () => {
+      console.log("opened");
     });
 
     console.log("created serial port");
     return await new Promise((resolve) => {
-      sp.on("data", (data) => {
+      port.on("error", (err) => {
+        console.error(`port error: ${err}`);
+      });
+
+      port.on("data", (data) => {
         console.log(`got data: ${data}`);
         resolve(data);
       });
