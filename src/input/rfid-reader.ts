ff78ab41ec22210ed1488e6482c76ab0c46ab01a
Jorge Martinez <jorgehmv@gmail.com>
2023-02-22 13:02:03 -0600

    trying node-hid

--- src/input/rfid-reader.ts
+++ src/input/rfid-reader.ts
@@ -1,33 +1,57 @@
 import { IReader } from "./reader";
-import { SerialPort } from "serialport";
+// import { SerialPort } from "serialport";
+import HID from "node-hid";
 
 export class RfidReader implements IReader {
+  // async read(): Promise<string> {
+  //   console.log("entered read");
+  //   console.log(`opening at ${process.env.USB_SERIAL_PATH}`);
+  //   var port = new SerialPort(
+  //     {
+  //       path: process.env.USB_SERIAL_PATH!,
+  //       baudRate: 57600,
+  //     },
+  //     () => {
+  //       console.log(`open callback: ${arguments}`);
+  //     }
+  //   );
+
+  //   port.on("open", () => {
+  //     console.log("opened");
+  //   });
+
+  //   console.log("created serial port");
+  //   return await new Promise((resolve) => {
+  //     port.on("error", (err) => {
+  //       console.error(`port error: ${err}`);
+
+  //     port.on("data", (data) => {
+  //       console.log(`got data: ${data}`);
+  //       resolve(data);
+  //     });
+  //   });
+  // }
+
   async read(): Promise<string> {
-    console.log("entered read");
-    console.log(`opening at ${process.env.USB_SERIAL_PATH}`);
-    var port = new SerialPort(
-      {
-        path: process.env.USB_SERIAL_PATH!,
-        baudRate: 57600,
-      },
-      () => {
-        console.log(`open callback: ${arguments}`);
-      }
-    );
+    const devices = HID.devices();
+    if (!process.env.USB_SERIAL_PATH && devices.length !== 1) {
+      throw new Error(
+        `not a single device to connect from. \nDevices count: ${devices.length}, USB_SERIAL_PATH: ${process.env.USB_SERIAL_PATH}`
+      );
+    }
+    const path = process.env.USB_SERIAL_PATH ?? devices[0].path!;
 
-    port.on("open", () => {
-      console.log("opened");
-    });
+    var device = new HID.HID(path);
 
-    console.log("created serial port");
     return await new Promise((resolve) => {
-      port.on("error", (err) => {
-        console.error(`port error: ${err}`);
+      device.on("data", function (data) {
+        console.log(`got data: ${data}`);
+        device.close();
+        resolve(data);
       });
 
-      port.on("data", (data) => {
-        console.log(`got data: ${data}`);
-        resolve(data);
+      device.on("error", function (err) {
+        console.error(err);
       });
     });
   }
