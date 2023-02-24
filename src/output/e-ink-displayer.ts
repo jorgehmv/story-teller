555852b9e45bd6a5a74ff1808e51e3f6cf727820
Jorge Martinez <jorgehmv@gmail.com>
2023-02-23 19:38:41 -0600

    include python files in build

--- src/output/e-ink-displayer.ts
+++ src/output/e-ink-displayer.ts
@@ -1,8 +1,8 @@
 import { IDisplayer, Mode } from "./displayer";
 import { spawn } from "child_process";
+import path from "path";
 
 let clearId: NodeJS.Timeout | null = null;
-let lastArgs: [string, Mode] = ["", "prompt"];
 
 export class EInkDisplayer implements IDisplayer {
   display(message: string, mode: Mode) {
@@ -12,7 +12,8 @@ export class EInkDisplayer implements IDisplayer {
   }
 
   private showText(text: string, mode: Mode) {
-    spawn("python3", ["./e-ink-lib/display.py", text, mode]);
+    const pythonFilePath = path.join(__dirname, "e-ink-lib", "display.py");
+    spawn("python3", [pythonFilePath, text, mode]);
   }
 
   private scheduleRefresh(text: string, mode: Mode) {
